70590e51b214902b06c9014567104172f20127ad
Ajihey77 <anjihey77@gmail.com>
2025-02-09 22:44:35 +0900

    ✏️ : fetch 를 해보자

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,6 +2,14 @@ export const metadata = {
   title: "Home | Nest Movies",
 };
 
-export default function page() {
-  return <div>page</div>;
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+async function getMovies() {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+}
+export default async function HomePage() {
+  const movies = await getMovies();
+  return <div>{JSON.stringify(movies)}</div>;
 }
